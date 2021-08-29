697defd3e5176191649691abdd7d8dc76f95d5ae
PradhuumnaPancholi <pnpancholi@protonmail.com>
2021-08-29 13:33:15 -0400

    added access control for default admin role and minter role

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -2,16 +2,35 @@
 pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol';
+import '@openzeppelin/contracts/access/AccessControl.sol';
 
-contract MyToken is ERC20 {
+contract MyToken is ERC20, ERC20Burnable, AccessControl {
+
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    uint256 public initialSupply = 1000;
 
     bool public mintable;
     bool public pausable;
 
-    uint256 public initialSupply = 1000;
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
         mintable = true;
         pausable = true;
+        _mint(msg.sender, initialSupply);
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(MINTER_ROLE, msg.sender);
     }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        require(mintable, "MyToken is no longer mintable");
+        _mint(to, amount);
+    }
+
+    
+
+    // function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override(ERC20, ERC20Pausable) {
+    //     super._beforeTokenTransfer(from, to, amount);
+    // }
 
 }
