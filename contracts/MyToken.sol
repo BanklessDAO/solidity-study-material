f292afb5d1248edd145883fdcce81bcc98bb9932
PradhuumnaPancholi <pnpancholi@protonmail.com>
2021-08-29 14:22:40 -0400

    added pause and unpause

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -4,33 +4,37 @@ pragma solidity ^0.8.0;
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import '@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol';
 import '@openzeppelin/contracts/access/AccessControl.sol';
+import '@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol';
 
-contract MyToken is ERC20, ERC20Burnable, AccessControl {
+contract MyToken is ERC20, ERC20Burnable, ERC20Pausable, AccessControl {
 
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256('PAUSER_ROLE');
 
     uint256 public initialSupply = 1000;
 
-    bool public mintable;
-    bool public pausable;
-
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
-        mintable = true;
-        pausable = true;
         _mint(msg.sender, initialSupply);
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _setupRole(MINTER_ROLE, msg.sender);
+        _setupRole(PAUSER_ROLE, msg.sender);
     }
 
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
-        require(mintable, "MyToken is no longer mintable");
         _mint(to, amount);
     }
 
-    
+    function pause() public onlyRole(PAUSER_ROLE){
+        _pause();
+    }
 
-    // function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override(ERC20, ERC20Pausable) {
-    //     super._beforeTokenTransfer(from, to, amount);
-    // }
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override(ERC20, ERC20Pausable) {
+        super._beforeTokenTransfer(from, to, amount);
+    }
 
 }
