97f232dbd781697c594aa1442d9d767af208c953
PradhuumnaPancholi <pnpancholi@protonmail.com>
2021-08-30 19:27:24 -0400

    added psuedo code and basse plan for addressbook

---
+++ contracts/AddressBook.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.0;
+
+contract AddressBook {
+
+    //struct for a type of "address" // this allows every user to have their address book//
+
+    //mapping to link user with their addressbook//
+
+    //function to add an address//
+    
+    //function to remove and address//
+
+    //function to get a user's addeess book//
+}
