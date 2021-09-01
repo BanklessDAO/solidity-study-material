4ec5192b4130490fe9f40d98af871767370eaca2
PradhuumnaPancholi <pnpancholi@protonmail.com>
2021-08-31 22:16:01 -0400

    added func to add and get aBook data

--- contracts/AddressBook.sol
+++ contracts/AddressBook.sol
@@ -4,12 +4,24 @@ pragma solidity ^0.8.0;
 contract AddressBook {
 
     //struct for a type of "address" // this allows every user to have their address book//
+    struct Record {
+        string name;
+        address eAddress;
+        string note;
+    }
 
     //mapping to link user with their addressbook//
+    mapping (address => Record[]) public _aBook;
 
     //function to add an address//
+    function addAddress(string memory _name, address _address,string memory _note) public   {
+        Record memory newRecord = Record(_name, _address, _note);
+        _aBook[msg.sender].push(newRecord);
+    }
     
-    //function to remove and address//
-
     //function to get a user's addeess book//
+    function getABook() public view returns (Record[] memory){
+        return _aBook[msg.sender];
+    }
+
 }
